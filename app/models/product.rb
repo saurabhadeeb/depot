059079cb829d49e311ebe1d5783fd02501311d98
saurabhadeeb <saurabh@scooze.com>
2014-01-17 13:01:59 +0530

    Added some validation to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
+  #All the text fields for product must be provided
+  validates :title, :description, :image_url, :presence => true
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|jpeg|png)\z}i,
+    :message => "must be a URL for a GIF, JPG or PNG image."
+  }
+  
+  # price has to be at least Re 1.
+  validates :price, :numericality => {:greater_than_or_equal_to => 1.00}
 end
